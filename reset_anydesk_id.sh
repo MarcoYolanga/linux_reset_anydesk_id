efd152378eb84e6dd92c4b824c7c3ec6e675b5af
Marco Bordino <marco@yolanga.it>
2020-09-18 18:15:06 +0200

    added running time limit, new password can be given as shell argument

--- reset_anydesk_id.sh
+++ reset_anydesk_id.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 echo "Reset Anydesk (Script v1.0)"
-read -p "Your Anydesk password: " pass
+if [ -z ${1+x} ]
+then
+	read -p "Your Anydesk password: " pass
+else
+	pass=$1
+fi
 actual_id=$(anydesk --get-id)
 if [ "$actual_id" == "" ]
 then
@@ -13,8 +18,15 @@ echo "Restarting anydesk..."
 service anydesk restart
 echo -n "Waiting new id.."
 new_id=$(anydesk --get-id)
+limit=10
 while [ "$new_id" == "$actual_id" ] || [ "$new_id" == "" ] || [ "$new_id" == "SERVICE_NOT_RUNNING" ]
 do
+	if [ "$limit" == 0 ]
+	then
+		echo "Timeout exceeded"
+		break
+	fi
+	((limit--))
         echo -n "."
         service anydesk restart
 	echo -n "."
